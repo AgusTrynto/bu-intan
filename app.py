062e56bcc94e31e679aa5c5e2bc33fcf2f19a6e0
AgusTrynto <117804878+AgusTrynto@users.noreply.github.com>
2025-05-27 10:13:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ custom_stopwords = [
     "menjadi", "lebih", "banyak", "memiliki", "dapat", "akan", "dengan",
     "adalah", "karena", "juga", "seperti", "dalam", "yang", "untuk", "oleh",
     "sudah", "masih", "namun", "hingga", "tanpa", "pada", "bahwa", "agar", 
-    "berbagai", "orang", "memberikan", "kompasiana", "komentar", "selanjutnya","ن","bukan","ا","satu"
+    "berbagai", "orang", "memberikan", "kompasiana", "komentar", "selanjutnya","ن","bukan","ا","satu","و","ي","ل"
 ]
 
 # Koneksi MongoDB
